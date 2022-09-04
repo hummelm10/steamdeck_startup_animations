26acc4cb9ff21d2f0738e0c5e3a82b71b0f7c365
Miguel Hummel <hummelm10@gmail.com>
2022-09-04 15:10:32 -0400

    updated sed to perl and added some extra checks for file clobbering

---
+++ install_dev.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#installs the feature/dev branch
+#you really shouldnt use this unless you know what youre doing. high chance of soft bricking
+
+# Create required directories
+echo ":: Creating required directories"
+mkdir -p "$HOME/homebrew"
+mkdir -p "$HOME/.config/systemd/user"
+
+# Clone the startup animations repository
+if [[ ! -d "$HOME/homebrew/startup_animations" ]]; then
+  echo ":: Installing to $HOME/homebrew/startup_animations"
+  git clone --branch feature/dev https://github.com/hummelm10/steamdeck_startup_animations "$HOME/homebrew/startup_animations"
+  cd "$HOME/homebrew/startup_animations"
+else
+  echo ":: Updating $HOME/homebrew/startup_animations"
+  cd "$HOME/homebrew/startup_animations"
+  git pull
+fi
+
+# Install the service file
+echo ":: Installing the startup service"
+ln -sf "$HOME/homebrew/startup_animations/randomize_deck_startup.service" "$HOME/.config/systemd/user/randomize_deck_startup.service"
+systemctl --user daemon-reload
+systemctl --user enable --now randomize_deck_startup.service
+
--- randomize_deck_startup.sh
+++ randomize_deck_startup.sh
@@ -52,7 +52,7 @@ check_backup_js_css() {
     else
       msg "Creating backup of initial library.css ($checksum)"
       cp "$DECK_CSS_FILE" "$DECK_CSS_FILE.backup"
-      cp "$DECK_CSS_FILE" "$HOME/homebrew/startup_animations/library.css.mod"
+      cp "$DECK_CSS_FILE" "$HOME/homebrew/startup_animations/library.css.modb"
     fi
   fi
 
@@ -60,6 +60,7 @@ check_backup_js_css() {
     checksum="$(md5sum "$DECK_JS_FILE" | cut -d ' ' -f 1)"
     if [[ "$checksum" != "$DECK_JS_STOCK_MD5" ]]; then
       msg2 "library.js has already been modified, cannot make a backup"
+      cp "$DECK_JS_FILE.backup" "$HOME/homebrew/startup_animations/library.js.modb"
     else
       msg "Creating backup of initial library.js ($checksum)"
       cp "$DECK_JS_FILE" "$DECK_JS_FILE.backup"
@@ -78,21 +79,33 @@ random_animation() {
 }
 
 mod_css() {
+  if [[ ! -f "$HOME/homebrew/startup_animations/library.css.modb" ]]; then
+    cp "$DECK_CSS_FILE.backup" "$HOME/homebrew/startup_animations/library.css.modb"
+  fi
+  cp "$HOME/homebrew/startup_animations/library.css.modb" "$HOME/homebrew/startup_animations/library.css.mod"
   msg "Modifying CSS file"
-  sed -i -E 's/^(.*img\{.*width\:)(300px)(.*height\:)(.*px)(.*$)/\10100%\3/' "$HOME/homebrew/startup_animations/library.css.mod"
-  sed -i -E 's/^(.*video\{.*width\:)(300px)(.*height\:)(.*px)(.*$)/\10100%\3/' "$HOME/homebrew/startup_animations/library.css.mod"
-  sed -i -E 's/^(!?.*animation-duration\:)(.*ms)(.*$)/\13000ms\3/' "$HOME/homebrew/startup_animations/library.css.mod"
-  sed -i -E 's/^(!?.*animation-delay\:)(.*ms)(.*$)/\111500ms\3/' "$HOME/homebrew/startup_animations/library.css.mod"
+  perl -p -i -e 's/^(.+?)\s(.*img\{.*?width\:)(.*?px)(.*?height\:)(.*?px)(.*?$)/${1}${2}0100%${4}0100%${6}/g' "$HOME/homebrew/startup_animations/library.css.mod"
+  perl -p -i -e 's/^(.+?)\s(.*video\{.*?width\:)(.*?px)(.*?height\:)(.*?px)(.*?$)/${1}${2}0100%${4}0100%${6}/g' "$HOME/homebrew/startup_animations/library.css.mod"
+  perl -p -i -e 's/^(!?.*animation-duration\:)(.*?ms)(.*$)/${1}3000ms${3}/g' "$HOME/homebrew/startup_animations/library.css.mod"
+  perl -p -i -e 's/^(!?.*animation-delay\:)(.*?ms)(.*$)/${1}11500ms${3}/g' "$HOME/homebrew/startup_animations/library.css.mod"
+  #remove comment line to make room for truncate
+  sed -i '/\/\*.*\*\// d; /\/\*/,/\*\// d' "$HOME/homebrew/startup_animations/library.css.mod"
+  truncate -s $DECK_CSS_FILE_SIZE "$HOME/homebrew/startup_animations/library.css.mod"
   msg "Enabled full screen animations"
   ln -f "$HOME/homebrew/startup_animations/library.css.mod" "$DECK_CSS_FILE"
   msg "Linked modified CSS file"
 }
 
 mod_js() {
+  if [[ ! -f "$HOME/homebrew/startup_animations/library.js.modb" ]]; then
+    cp "$DECK_JS_FILE.backup" "$HOME/homebrew/startup_animations/library.js.modb"
+  fi
+  cp "$HOME/homebrew/startup_animations/library.js.modb" "$HOME/homebrew/startup_animations/library.js.mod"
   msg "Modifying JS file"
   sed -i -E 's/(.*return Object\(f\.y\)\()(i,1e4)(.*$)/\1i,9e9\3/' "$HOME/homebrew/startup_animations/library.js.mod"
   msg "Modified time limit"
   sed -i -E -E 's/(.*)(HapticEvent\(0,2,6,2,0\))(.*$)/\1HapticEvent\(0,0,0,0,0\)\3/' "$HOME/homebrew/startup_animations/library.js.mod"
+  truncate -s $DECK_JS_FILE_SIZE "$HOME/homebrew/startup_animations/library.js.mod"
   msg "Disabled boot haptics"
   ln -f "$HOME/homebrew/startup_animations/library.js.mod" "$DECK_JS_FILE"
   msg "Linked modified JS file"
